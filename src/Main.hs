079c6c9d0cec823bec44c557828c138a76a2d020
Mark Lavrentyev <lavrema@outlook.com>
2023-04-29 16:42:13 -0700

    Add temporary imports to force all files to be compiled

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
 module Main (main) where
 
+import Lang.Datalog.Ast ()
+import Lang.Datalog.Parser ()
+import Lang.Datalog.Tokenizer ()
+import Lang.Datalog.Interpreter ()
+
 main :: IO ()
 main = do
   putStrLn "hello world"
