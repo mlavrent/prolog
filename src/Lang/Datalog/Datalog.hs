ea4dc82d224d8a4bb8fef233af95cbbaf9e0a417
Mark Lavrentyev <lavrema@outlook.com>
2023-04-30 14:53:17 -0700

    Add datalog program state

--- src/Lang/Datalog/Datalog.hs
+++ src/Lang/Datalog/Datalog.hs
@@ -1,16 +1,27 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Lang.Datalog.Datalog where
 
-import Interpret.Interpreter (Program)
-import Parse.Parser (Parsable, parse)
+import Interpret.Interpreter (Program (interpret))
+import Parse.Parser (Parsable (parse))
 import Lang.Datalog.Tokens (DatalogToken)
 import qualified Lang.Datalog.Ast as Ast
+import Interpret.ProgramState (HasInitialState (initialState))
+import Lang.Datalog.Ast (Clause)
 
 -- Wrappers around the tokenized and parsed program to carry extra context (e.g. filename) if needed
-data DatalogToks i v = DatalogToks { tokens :: [DatalogToken i v] }
-data DatalogProgram i v = DatalogProgram { program :: Ast.Program i v }
+newtype DatalogToks i v = DatalogToks { tokens :: [DatalogToken i v] }
+newtype Datalog i v = Datalog { program :: Ast.Program i v }
 
-instance Parsable (DatalogToks i v) (DatalogProgram i v) where
+instance Parsable (DatalogToks i v) (Datalog i v) where
   parse = undefined -- TODO:
 
--- instance Program (DatalogProgram i v)
+
+data DatalogState i v = ProgramState
+  { importedReqs :: [Ast.Id i]
+  , clauses :: [Clause i v] }
+
+instance HasInitialState (DatalogState i v) where
+  initialState = ProgramState [] []
+
+instance Program (Datalog i v) (DatalogState i v) String where
+  interpret p = undefined
