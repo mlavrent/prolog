639d16c26452f4c34eda01ee19de35c029ecfd81
Mark Lavrentyev <lavrema@outlook.com>
2023-04-29 22:12:12 -0700

    Write interpreter type class

--- src/Interpret/Interpreter.hs
+++ src/Interpret/Interpreter.hs
@@ -1,6 +1,6 @@
 module Interpret.Interpreter where
+import Control.Monad.State (State)
 
-import Control.Monad.State.Lazy (State)
-
-class Interpretable p where
-  interpret :: State s p
+class Program p where
+  -- interpretting a program is a computation from an init state to a final state and result value
+  interpret :: p s o -> State s o
