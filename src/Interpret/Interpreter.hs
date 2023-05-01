b241b3b8ac5a64e0dddc50c6941a1e6a401213d3
Mark Lavrentyev <lavrema@outlook.com>
2023-04-30 20:26:29 -0700

    Rename interpretable typeclass

--- src/Interpret/Interpreter.hs
+++ src/Interpret/Interpreter.hs
@@ -1,9 +1,10 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Interpret.Interpreter where
 
 import Control.Monad.State (State, runState)
 import Interpret.ProgramState (HasInitialState, initialState)
 
-class Program p s r where
+class Interpretable p s r where
   -- interpretting a program is a computation from an init state to a final state and result value
   interpret :: p -> State s r
 
